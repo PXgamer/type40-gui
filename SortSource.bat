ba585f92e73687cd947a1d309f08f0e404d3f557
PXgamer <owzie123@gmail.com>
2015-11-11 19:57:22 +0000

    Updated to check uTorrent for "Sources" label

--- SortSource.bat
+++ SortSource.bat
@@ -1,6 +1,17 @@
-@echo off &setlocal
+@echo off
 set dir=%1
+set label=%2
 set "extlist=.mkv|.mp4|.bat|"
+
+@echo off
+if /I %label% equ "Sources" (
+ 	    GOTO isSource
+) else (
+ 	GOTO end
+)
+
+:isSource
+echo is a Source
 for /r %dir% %%i in (*) do (
     set "fname=%%~i"
     setlocal enabledelayedexpansion
@@ -10,6 +21,8 @@ for /r %dir% %%i in (*) do (
 
 for /f "usebackq delims=" %%d in (`"dir /ad/b/s | sort /R"`) do rd "%%d"
 
-CLS
 echo All Excess Files Removed
 echo %dir% cleaned
+
+:end
+echo Finished
