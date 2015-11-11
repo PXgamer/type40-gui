5bf6367d3d05756b62896d255aa76c9f200c4c16
PXgamer <owzie123@gmail.com>
2015-11-11 13:49:18 +0000

    I forgot to add the actual directory input, and remove echo from my dev testing. Fully functional now.

--- SortSource.bat
+++ SortSource.bat
@@ -1,12 +1,15 @@
 @echo off &setlocal
+set dir=%1
 set "extlist=.mkv|.mp4|.bat|"
-for /r "c:\video" %%i in (*) do (
+for /r %dir% %%i in (*) do (
     set "fname=%%~i"
     setlocal enabledelayedexpansion
-    if "!extlist:%%~xi|=!"=="%extlist%" echo del "!fname!"
+    if "!extlist:%%~xi|=!"=="%extlist%" del "!fname!"
     endlocal
 )
 
 for /f "usebackq delims=" %%d in (`"dir /ad/b/s | sort /R"`) do rd "%%d"
 
+CLS
 echo All Excess Files Removed
+echo %dir% cleaned
